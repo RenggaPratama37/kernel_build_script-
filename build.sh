07925c2f74034b6ff4043bdf843a91681a638de6
RenggaPratama37 <renggaronapratama@gmail.com>
2023-12-14 23:32:07 +0700

    build.sh: make custom clang optional

--- build.sh
+++ build.sh
@@ -11,11 +11,30 @@ fi
 # Let's enter the directory
 echo "Enter kernel_xiaomi_lime"   
 cd kernel_xiaomi_lime
+kernel_direktory="$(pwd)" 
 
-# Export Neutron Clang 
-echo "Export Neutron Clang to the PATH"
-export PATH="$HOME/toolchains/neutron-clang/bin:$PATH"
-
+# Compile Preparation
+    cd "$HOME"
+    sudo apt install bc
+    sudo apt install flex
+    sudo apt install zstd
+    sudo apt install libarchive-tools
+# Option Neutron clang as a Custom Clang Compile
+if [ "$response" == "y" ]  || [ "$response" == "Y" ]; then
+    echo "Preparing Neutron Clang Environtment..."
+    if [ -d "$HOME/toolchains/neutron-clang" ]; then
+        echo "Neutron clang has been installed"
+    else
+        echo "Installing Neutron Clang"         
+        mkdir -p "$HOME/toolchains/neutron-clang"
+        cd "$HOME/toolchains/neutron-clang"
+        curl -LO "https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman"
+        sudo chmod +x antmant
+        ./antman -S
+    fi
+    cd "kernel_directory"    
+    export PATH="$HOME/toolchains/neutron-clang/bin:$PATH"
+fi
 # Let's make a defconfig
 echo "importing defconfig"
 make O=/data/out ARCH=arm64 vendor/bengal-perf_defconfig
