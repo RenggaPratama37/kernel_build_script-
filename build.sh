29aba1b8276bcfa68b5159583852f4eb87a3cba4
RenggaPratama37 <renggaronapratama@gmail.com>
2023-12-15 06:55:27 +0700

    build.sh: zip the kernel

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 # Kernel Script Compiler by Rengga Pratama
 echo "Kernel Script Compiler by Rengga Pratama"
 # exit build directory
+script_directory="$(pwd)"
 cd ..
 workspace_directory="$(pwd)"
 
@@ -61,8 +62,17 @@ make O=/$workspace_directory/out ARCH=arm64 vendor/bengal-perf_defconfig
 # Let's start the build
 make -j$(nproc --all) O=/$workspace_directory/out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
+# Zipping the Image
+cd ..
+cp $script_directory/Anykernel3/ 
+cp out/arch/arm64/boot/Image Anykernel3
+cd Anykernel3
+rm -r .github
+rm .git  
+zip Anykernel3
+rm -r Anykernel3
 
+# Renaming zip
 
-
-
+mv Anykernel3.zip "renium_$(date '+%d%m%y%H%M').zip
 
