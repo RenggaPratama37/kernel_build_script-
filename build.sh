aa22ba26279ec413b13c9fb819b4a67d834dda2b
RenggaPratama37 <renggaronapratama@gmail.com>
2023-12-16 23:00:48 +0700

    build.sh: fix cuting file

--- build.sh
+++ build.sh
@@ -59,16 +59,16 @@ make O=/$workspace_directory/out ARCH=arm64 vendor/bengal-perf_defconfig
 make -j$(nproc --all) O=/$workspace_directory/out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
 # Zipping the Image
-cd ..
+cd "$workspace_directory"
 mkdir -p AnyKernel3
-cp -r "$script_directory"/AnyKernel3/*   AnyKernel3/
+cp -r "$script_directory"/AnyKernel3/* AnyKernel3/
 cp out/arch/arm64/boot/Image AnyKernel3/
-zip -r AnyKernel3.zip/* AnyKernel3 
+cd AnyKernel3
+zip -r AnyKernel3.zip *
+mv AnyKernel3.zip ..
+cd "$workspace_directory"
 
 # Renaming zip
-mv AnyKernel3.zip "renium_$(date '+%d%m%y%H%M')".zip
+mv AnyKernel3.zip "renium_$(date '+%d%m%y%H%M').zip"
 
 # Clean up file after building
-
-rm -r AnyKernel3
-rm -r "$workspace_directory"/out
