a351f956de032aba676c316845a4be44bfcd6ef0
RenggaPratama37 <renggaronapratama@gmail.com>
2024-03-23 21:41:54 +0700

    build.sh: Build under /Build

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
 # Let's make a defconfig
 echo "importing defconfig"
-make O=/$script_directory/out ARCH=arm64 vendor/bengal-perf_defconfig
+make O=/$script_directory/Build/out ARCH=arm64 vendor/bengal-perf_defconfig
 
 # Let's start the build
 while true;
@@ -40,10 +40,10 @@ do
     read -p "Using LLVM and LLVM_IAS? (Y/n/x (for exit)):" response
     if [ "$response" == "y" ]  || [ "$response" == "Y" ]; 
     then
-        make -j$(nproc --all) O=/$script_directory/out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1
+        make -j$(nproc --all) O=/$script_directory/Build/out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- LLVM=1 LLVM_IAS=1
     elif [ "$response" == "n" ]  || [ "$response" == "N" ];
     then
-        make -j$(nproc --all) O=/$script_directory/out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+        make -j$(nproc --all) O=/$script_directory/Build/out ARCH=arm64 CC=clang CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
     elif [ "$response" == "x" ];
     then
         echo "Exiting script."
@@ -54,32 +54,36 @@ do
     fi
 
     # Periksa apakah kompilasi selesai
-    if [ -f "$script_directory/out/arch/arm64/boot/Image.gz" ]; then
+    if [ -f "$script_directory/Build/out/arch/arm64/boot/Image.gz" ]; then
         echo "Build completed successfully."
         break
+    else
+        echo "Build Error"
+        break    
     fi
 done
 
-if [ -f "$script_directory/out/arch/arm64/boot/Image.gz" ];
+if [ -f "$script_directory/Build/out/arch/arm64/boot/Image.gz" ];
 then
     # Zipping the Image
     cd "$script_directory"
-    mkdir -p kernel
-    cp -r "$script_directory"/AnyKernel3/* kernel/
-    cp out/arch/arm64/boot/Image kernel/
-    cd kernel
+    mkdir -p Build/kernel
+    cp -r "$script_directory"/AnyKernel3/* Build/kernel/
+    cp Build/out/arch/arm64/boot/Image Build/kernel/
+    cd Build/kernel/
     zip -r kernel *
 
     # Renaming zip
     echo "renaming zip"
-    mv kernel.zip $script_directory
-    cd $script_directory
+    mv kernel.zip $script_directory/Build
+    cd $script_directory/Build
     mv kernel.zip "renium_$(date '+%d%m%y%H%M').zip"
 
     # Clean up file after building
     echo "cleaning file after build completed"
-    rm -r out
+    rm -r out        
     rm -r kernel
+    cd $script_directory
 else
     echo "Build Failed"
     cd $script_directory
